99db6f3afc6f6db0c4069f440a8e3ca02e168108
NagendraGloify <nagendra@gloify.com>
2024-07-29 19:04:43 +0530

    for testing disabled s3

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,16 +1,16 @@
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+# Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 
-if Rails.env.production?
-  Paperclip::Attachment.default_options.merge!({
-    storage: :s3,
-    s3_credentials: {
-      bucket: ENV.fetch('AWS_S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_region: ENV.fetch('AWS_REGION'),
-    },
-    s3_permissions: 'private',
-    s3_protocol: 'https'
-  })
-end
+# if Rails.env.production?
+#   Paperclip::Attachment.default_options.merge!({
+#     storage: :s3,
+#     s3_credentials: {
+#       bucket: ENV.fetch('AWS_S3_BUCKET_NAME'),
+#       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+#       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+#       s3_region: ENV.fetch('AWS_REGION'),
+#     },
+#     s3_permissions: 'private',
+#     s3_protocol: 'https'
+#   })
+# end
